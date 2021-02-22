438ae443beb31dc6ba80608a66bbbb3a67e5b638
diego6ruiz <55885621+diego6ruiz@users.noreply.github.com>
2021-02-21 23:26:09 -0600

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,21 +1,20 @@
 
 public class BubbleSort {
 	public BubbleSort() {
-		System.out.println("Bubble Sort:");
 	}
 	
-	public void bubbleSort(int arr[]) 
-    { 
+	public void bubbleSort(int arr[]) { 
         int n = arr.length; 
         for (int i = 0; i < n-1; i++) 
             for (int j = 0; j < n-i-1; j++) 
                 if (arr[j] > arr[j+1]) 
                 { 
-                    // swap arr[j+1] and arr[j] 
+                    //Intercambio simple 
                     int temp = arr[j]; 
                     arr[j] = arr[j+1]; 
                     arr[j+1] = temp; 
-                } 
+                }
+
     } 
 
 }
