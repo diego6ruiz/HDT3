38c91ec3f60cde04064ec683aac96fa8565c7deb
diego6ruiz <55885621+diego6ruiz@users.noreply.github.com>
2021-02-21 23:25:20 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,53 +1,113 @@
+/*
+ * Diego Ruiz
+ * Javier Alvarez
+ * 
+ * Driver program
+ * Hoja de Trabajo 3
+ * Sort con metodos Gnome, Merge, Radix, Quick y Bubble.
+ * 
+*/
 
+import java.util.ArrayList;
 
 public class Main {  
 	public static void main(String[] args) {
-		int[] array = {5, 4, 2, 9, 1, 6, 3, 8, 10, 7};
+		//n define el numero de elementos con los que se trabaja
+		int n = 10;
+		//Listas para Gnome Sort
+		ArrayList<Integer> arrayGnome = new ArrayList<Integer>();
+		int[] intArrayGnome = {};
+		//Listas para Merge Sort
+		ArrayList<Integer> arrayMerge = new ArrayList<Integer>();
+		int[] intArrayMerge = {};
+		//Listas para Quick Sort
+		ArrayList<Integer> arrayQuick = new ArrayList<Integer>();
+		int[] intArrayQuick = {};
+		//Listas para Radix Sort
+		ArrayList<Integer> arrayRadix = new ArrayList<Integer>();
+		int[] intArrayRadix = {};
+		//Listas para Bubble Sort
+		ArrayList<Integer> arrayBubble = new ArrayList<Integer>();
+		int[] intArrayBubble = {};
 		
-		GnomeSort gs = new GnomeSort();
+		ArrayFromTXT at = new ArrayFromTXT();
+		ConvertIntegers ci = new ConvertIntegers();
 		
-		for (int i: gs.gnomeSort(array, array.length)) {
+		TXTGenerator ag = new TXTGenerator();
+		ag.generateRandomTXT(n);
+        
+        //Proceso de ordenamiento Gnome Sort
+		//retorno de txt ordenado
+		arrayGnome = at.generateArray("random.txt");
+		intArrayGnome = ci.convertIntegers(arrayGnome);
+        GnomeSort gs = new GnomeSort();
+		gs.gnomeSort(intArrayGnome, n);
+		for (int i: intArrayGnome) {
             System.out.println(i);
         }
-		
-		
-		int[] arra = {5, 4, 2, 9, 1, 6, 3, 8, 10, 7};
-		
-		MergeSort ms = new MergeSort();
-		ms.mergeSort(arra, 0, arra.length-1);
-		
-		
-        for (int i: arra) {
+        //Ordenamiento de txt previamente Ordenado
+        gs.gnomeSort(intArrayGnome, n);
+        for (int i: intArrayGnome) {
+            System.out.println(i);
+        }
+         
+		//Proceso de ordenamiento Merge Sort
+		//retorno de txt ordenado
+        arrayMerge = at.generateArray("random.txt");
+        intArrayMerge = ci.convertIntegers(arrayMerge);
+        MergeSort ms = new MergeSort();
+        ms.mergeSort(intArrayMerge, 0, intArrayMerge.length-1);
+        for (int i: intArrayMerge) {
+            System.out.println(i);
+        }
+        //Ordenamiento de txt previamente Ordenado
+        ms.mergeSort(intArrayMerge, 0, intArrayMerge.length-1);
+        for (int i: intArrayMerge) {
             System.out.println(i);
         }
         
-        int[] arr = {5, 4, 2, 9, 1, 6, 3, 8, 10, 7};
-        
+		//Proceso de ordenamiento Quick Sort
+		//retorno de txt ordenado
+        arrayQuick = at.generateArray("random.txt");
+        intArrayQuick = ci.convertIntegers(arrayQuick);
         QuickSort qs = new QuickSort();
-		qs.quickSort(arr, 0, array.length-1);
-		
-		
-        for (int i: arr) {
+		qs.quickSort(intArrayQuick, 0, intArrayQuick.length-1);
+        for (int i: intArrayQuick) {
+            System.out.println(i);
+        }
+        //Ordenamiento de txt previamente Ordenado
+        qs.quickSort(intArrayQuick, 0, intArrayQuick.length-1);
+        for (int i: intArrayQuick) {
             System.out.println(i);
         }
         
-        int[] ar = {5, 4, 2, 9, 1, 6, 3, 8, 10, 7};
-        
+		//Proceso de ordenamiento Radix Sort
+		//retorno de txt ordenado
+        arrayRadix = at.generateArray("random.txt");
+        intArrayRadix = ci.convertIntegers(arrayRadix);
         RadixSort rs = new RadixSort();
-		rs.radixSort(ar);
-		
-		
-        for (int i: ar) {
+		rs.radixSort(intArrayRadix, n);
+        for (int i: intArrayRadix) {
+            System.out.println(i);
+        }
+        //Ordenamiento de txt previamente Ordenado
+        rs.radixSort(intArrayRadix, n);
+        for (int i: intArrayRadix) {
             System.out.println(i);
         }
         
-        int[] a = {5, 4, 2, 9, 1, 6, 3, 8, 10, 7};
-        
+		//Proceso de ordenamiento Bubble Sort
+		//retorno de txt ordenado
+        arrayBubble = at.generateArray("random.txt");
+        intArrayBubble = ci.convertIntegers(arrayBubble);
         BubbleSort bs = new BubbleSort();
-		bs.bubbleSort(a);
-		
-		
-        for (int i: a) {
+		bs.bubbleSort(intArrayBubble);
+        for (int i: intArrayBubble) {
+            System.out.println(i);
+        }
+        //Ordenamiento de txt previamente Ordenado
+        bs.bubbleSort(intArrayBubble);
+        for (int i: intArrayBubble) {
             System.out.println(i);
         }
 		
